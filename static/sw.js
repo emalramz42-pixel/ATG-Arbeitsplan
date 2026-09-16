95f0a5777785228a7e396cb2e0124233161d7e30
Emal Ramz <emal.ramz@jtl-software.com>
2026-09-16 15:45:54 +0200

    Service Worker nur GET-Requests abfangen lassen (behebt haengende POST-Formulare auf iOS Safari)

--- static/sw.js
+++ static/sw.js
@@ -3,5 +3,11 @@
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
 self.addEventListener("fetch", (event) => {
+    // Nur GET-Anfragen durchreichen. POST-Formulare (Speichern, Entfernen, ...)
+    // werden dem Browser ueberlassen, da abgefangene POST-Weiterleitungen
+    // in Safari/iOS zu haengenden Formular-Sends fuehren koennen.
+    if (event.request.method !== "GET") {
+        return;
+    }
     event.respondWith(fetch(event.request));
 });
